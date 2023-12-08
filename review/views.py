4b64942ad5a06b655e45776f0658835d235abd08
DimasHN_04 <dimashn04@gmail.com>
2023-12-09 02:08:49 +0700

    Fixes at @review:views.py

--- review/views.py
+++ review/views.py
@@ -265,7 +265,7 @@ def delete_review_ajax(request, review_id):
 
 @csrf_exempt
 @login_required(login_url='/auth/login')
-def post_delete_review_flutter(request):
+def delete_review_flutter(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         review_id = data["review-id"]
@@ -288,33 +288,7 @@ def post_delete_review_flutter(request):
                 book_rating.delete()
 
             return JsonResponse({"status": "success"}, status=204)
-        return HttpResponse("Unauthorized", status=401) 
-    return JsonResponse({"status": "error"}, status=401)
-
-@csrf_exempt
-@login_required(login_url='/auth/login')
-def delete_review_flutter(request, review_id):
-    if request.method == 'DELETE':
-        review = get_object_or_404(Review, pk=review_id)
-
-        if request.user == review.user:
-            book = review.book
-
-            review.delete()
-
-            book_rating = BookRating.objects.get(book=book)
-            total_reviews = Review.objects.filter(book=book).count()
-            if total_reviews > 0:
-                total_ratings = sum([review.rating for review in Review.objects.filter(book=book)])
-                book_rating.rating = total_ratings / total_reviews
-                book_rating.save()
-            else:
-                book_rating.rating = 0
-                book_rating.delete()
-
-            return JsonResponse({"status": "success"}, status=204)
-        else:
-            return JsonResponse({"status": "unauthorized"}, status=401) 
+        return JsonResponse({"status": "unauthorized"}, status=204)
     return JsonResponse({"status": "error"}, status=401)
 
 @csrf_exempt
